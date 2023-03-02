63b7ad22d5c3d93b9a73731018197db708099f57
Serhii Rudenko <svr.phoenix@gmail.com>
2023-03-02 19:41:39 +0100

    Added Task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+}
+
+const defaultBackgroundColor = refs.body.style.backgroundColor;
+let timerId = null;
+
+refs.btnStop.disabled = true;
+
+refs.btnStart.addEventListener('click', onStartBtnClick);
+refs.btnStop.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  const { btnStart, btnStop, body } = refs;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor()
+  }, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+}
+
+function onStopBtnClick() {
+  const { btnStart, btnStop, body } = refs;
+  clearInterval(timerId);
+  body.style.backgroundColor = defaultBackgroundColor;
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
